171c6a68046252dfa5bc700858d8dd29a2585d73
heneralovsem <semm297@gmail.com>
2023-10-20 19:57:50 +0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { join } from 'path';
             envFilePath: `.${process.env.NODE_ENV}.env`
         }),
         ServeStaticModule.forRoot({
-            rootPath: join(__dirname, '..', 'static')
+            rootPath: join(__dirname, 'dist/src/static')
         }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
